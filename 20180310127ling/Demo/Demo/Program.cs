d644fd5fa28c64657f413670d37225fb7c6cfc3c
20180310127ling <58196840+20180310127ling@users.noreply.github.com>
2019-11-26 11:37:41 +0800

    Add files via upload

---
+++ 20180310127ling/Demo/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!  你好 ！");
+            Console.WriteLine("Hello World! 您好！ ");
+            Console.ReadLine();
+        }
+    }
+}
